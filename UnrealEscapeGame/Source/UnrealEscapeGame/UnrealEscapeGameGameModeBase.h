915b1b4b08128f6f0a1a739e7b6ab8c737fe7677
Nasyrov Takhir <64357944+nasyrovt@users.noreply.github.com>
2022-02-14 01:12:25 +0100

    Initial Commit

---
+++ UnrealEscapeGame/Source/UnrealEscapeGame/UnrealEscapeGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealEscapeGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALESCAPEGAME_API AUnrealEscapeGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
